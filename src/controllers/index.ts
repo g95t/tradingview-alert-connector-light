534f8054736d93fc284981b5b46f6c109ae5e53a
g95t <144741189+g95t@users.noreply.github.com>
2026-06-06 18:18:34 +0200

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -18,8 +18,8 @@ router.post('/', async (req, res) => {
             return;
         }
 
-        if (!alertMessage.size || (alertMessage.order !== 'buy' && alertMessage.order !== 'sell')) {
-            console.error('Missing size or invalid order direction');
+        if (!alertMessage.price || price <= 0 || !isFinite(price) || !alertMessage.market || !alertMessage.size || (alertMessage.order !== 'buy' && alertMessage.order !== 'sell')) {
+            console.error('Error');
             res.send('Error');
             return;
         }
